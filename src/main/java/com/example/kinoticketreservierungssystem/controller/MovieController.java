6222fa695032375e6b26fe4274694d937c4a0663
Jannis Jüngert <73344489+juengeja@users.noreply.github.com>
2021-10-02 12:58:16 +0200

    Testing example

--- src/main/java/com/example/kinoticketreservierungssystem/controller/MovieController.java
+++ src/main/java/com/example/kinoticketreservierungssystem/controller/MovieController.java
@@ -1,6 +1,5 @@
 package com.example.kinoticketreservierungssystem.controller;
 
-
 import com.example.kinoticketreservierungssystem.entity.Movie;
 import com.example.kinoticketreservierungssystem.repository.MovieRepository;
 import org.springframework.http.HttpStatus;
@@ -10,32 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@RequestMapping("/api")
+@RequestMapping("/movies")
 @RestController
 @CrossOrigin(origins = "*")
 public class MovieController {
 
     private final MovieRepository movieRepository;
-    private static List<Movie> movies = new ArrayList<>();
 
     public MovieController(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
     }
 
-    @GetMapping("/moviedata")
+
+    @GetMapping("/getAll")
     public ResponseEntity<Iterable<Movie>> getAllMovies(
     ) {
-        Movie movie = new Movie();
-        movie.setName("James Bond");
-        movie.setDuration(162);
-        movie.setDescription("Beschreibung des Films James Bond");
-        movie.setGenre("Action");
-
-        movies.add(movie);
-
-        return new ResponseEntity<>(movies, HttpStatus.OK);
+        return new ResponseEntity<>(movieRepository.findAll(), HttpStatus.OK);
     }
 }
