5c066f03bb1ceae33015a73b7fc1d393c967daed
Jannis Jüngert <73344489+juengeja@users.noreply.github.com>
2021-10-02 13:16:08 +0200

    Update

--- src/main/java/com/example/kinoticketreservierungssystem/controller/MovieController.java
+++ src/main/java/com/example/kinoticketreservierungssystem/controller/MovieController.java
@@ -1,7 +1,6 @@
 package com.example.kinoticketreservierungssystem.controller;
 
 import com.example.kinoticketreservierungssystem.entity.Movie;
-import com.example.kinoticketreservierungssystem.repository.MovieRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,12 +16,7 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 public class MovieController {
 
-    private final MovieRepository movieRepository;
     private static List<Movie> movies = new ArrayList<>();
-
-    public MovieController(MovieRepository movieRepository) {
-        this.movieRepository = movieRepository;
-    }
 
     @GetMapping("/moviedata")
     public ResponseEntity<Iterable<Movie>> getAllMovies(
