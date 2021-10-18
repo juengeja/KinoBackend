ea0eb984dc8b8b1b3eeb6fb665a56541bddaf36d
binhdich <90770330+binhdich@users.noreply.github.com>
2021-10-18 14:21:50 +0200

    PathVariable Updated

--- src/main/java/com/example/kinoticketreservierungssystem/repository/MovieRepository.java
+++ src/main/java/com/example/kinoticketreservierungssystem/repository/MovieRepository.java
@@ -1,7 +1,6 @@
 package com.example.kinoticketreservierungssystem.repository;
 
 import com.azure.spring.data.cosmos.repository.CosmosRepository;
-import com.azure.spring.data.cosmos.repository.ReactiveCosmosRepository;
 import com.example.kinoticketreservierungssystem.entity.Movie;
 import org.springframework.stereotype.Repository;
 
