758d5020c811cd6ac3b120c343e8685efd41a77d
Tobias Fitzke <tobias.fitzke@dxc.com>
2021-10-18 10:13:34 +0200

    Update MovieRepository.java

--- src/main/java/com/example/kinoticketreservierungssystem/repository/MovieRepository.java
+++ src/main/java/com/example/kinoticketreservierungssystem/repository/MovieRepository.java
@@ -3,8 +3,9 @@ package com.example.kinoticketreservierungssystem.repository;
 import com.azure.spring.data.cosmos.repository.ReactiveCosmosRepository;
 import com.example.kinoticketreservierungssystem.entity.Movie;
 import org.springframework.stereotype.Repository;
-
+/*
 @Repository
 public interface MovieRepository extends ReactiveCosmosRepository<Movie, String> {
 
 }
+*/
