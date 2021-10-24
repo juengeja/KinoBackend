9f36a009256912332288d9a6ea3467594f49831a
binhdich <90770330+binhdich@users.noreply.github.com>
2021-10-24 18:13:26 +0200

    Send Email

---
+++ src/main/java/com/example/kinoticketreservierungssystem/service/SendMail.java
@@ -0,0 +1,50 @@
+package com.example.kinoticketreservierungssystem.service;
+
+
+import com.example.kinoticketreservierungssystem.blSupport.Mail;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class SendMailServiceImpl {
+    private final JavaMailSender javaMailSender;
+
+    public SendMailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+
+    public void sendMail(Mail mail) {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(mail.getRecipient(), mail.getRecipient());
+
+        msg.setSubject(mail.getSubject());
+        msg.setText(mail.getMessage());
+
+        javaMailSender.send(msg);
+    }
+
+
+    public void sendMailWithAttachments(Mail mail) throws MessagingException {
+        MimeMessage msg = javaMailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+
+        helper.setTo("to_@email");
+
+        helper.setSubject("Testing from Spring Boot");
+
+        helper.setText("Find the attached image", true);
+
+        helper.addAttachment("hero.jpg", new ClassPathResource("hero.jpg"));
+
+        javaMailSender.send(msg);
+    }
+}
